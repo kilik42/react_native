5f32f1029a7a931bd4eb27d2b97311a13acefa25
marvin evins <marvinevins@yahoo.com>
2024-04-13 20:35:23 -0500

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+import {Link} from 'expo-router'
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{color: 'blue'}} to="profile">profile</Link>
+    </View>
+  );
+}
+
+
